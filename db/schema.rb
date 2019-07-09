e068886efbee06c08ddf27f2e44009e4f3f15bc5
ellevalentine <elle.valentine@hotmail.com>
2019-07-09 14:06:22 +0100

    balance

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20190708193950) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
