6ae462c2aaa34270acef3ac0746ca75098f3735d
ellevalentine <elle.valentine@hotmail.com>
2019-07-09 12:11:31 +0100

    modal not complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,20 @@ user4 =User.create(username: "Tom", password: "123456")
 user5 =User.create(username: "Ben", password: "123456")
 
 Item.create(name: 'Postion', description: 'lalalal', user_id: user1.id )
-Item.create(name: 'HEllo', description: 'heheh', user_id: user2.id )
+Item.create(name: 'HEllo', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'hbdusbhjs', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'dog', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'cat', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'shoe', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'hat', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'dress', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'toy', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'baby', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'bat', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'park', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+Item.create(name: 'wedding', description: 'heheh', goal: 200, date:"12/09/2020", user_id: user2.id )
+
+
+
 
 
