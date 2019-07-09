5e678c096cc2995a44c1bd81c425e3b23fc1831b
github username <github email address>
2019-07-09 13:43:48 +0100

    Update charity_users_controller.rb

--- app/controllers/charity_users_controller.rb
+++ app/controllers/charity_users_controller.rb
@@ -1,5 +1,29 @@
 class CharityUsersController < ApplicationController
-    //show which charities they have donated to 
-
+    before_action :find_charity_user, only: [:show]
     
+    def show
+        render json: @charity_user
+    end 
+
+    def new
+        @charity_user = CharityUser.new
+    end
+
+    def create 
+        @charity_user = CharityUser.new(charity_user_params)
+        @charity_user.save
+        render json: @charity_user
+    end 
+
+
+    private 
+
+    def charity_user_params
+        params.require(:charity_user).permit(:user_id, :charity_id, :donation)
+    end
+
+    def find_charity_user
+        @charity_user = CharityUser.find(params[:id])
+    end
+
 end
