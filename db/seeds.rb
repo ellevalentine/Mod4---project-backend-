bf3ce71a35ee1b2828098609afa839d92995ad26
github username <github email address>
2019-07-09 12:27:23 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ user2 =User.create(username: "Leah", password: "123456")
 user3 =User.create(username: "Ranya", password: "123456")
 user4 =User.create(username: "Tom", password: "123456")
 user5 =User.create(username: "Ben", password: "123456")
+user5 =User.create(username: "Ines", password: "ines")
 
 Item.create(name: 'Postion', description: 'lalalal', user_id: user1.id )
 Item.create(name: 'HEllo', description: 'heheh', user_id: user2.id )
